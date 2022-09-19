8aed1ee166369a12a9f06d084790ba04595bf0c4
Santiago Aristi <santiago.aristi@mykenzie.snhu.edu>
2022-09-19 18:42:32 -0400

    Second Commit with Main.java

--- Main.java
+++ Main.java
@@ -1,2 +1,24 @@
-package PACKAGE_NAME;public class Main {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        String[] newString = wordsThatHaveLessThanFiveLetters("You can write this function");
+        System.out.println(Arrays.toString(newString));
+
+    }
+
+    public static String[] wordsThatHaveLessThanFiveLetters(String s) {
+        String[] arrayOfWords = s.split(" ");
+        List<String> listOfWords = new ArrayList<>(Arrays.asList(arrayOfWords));
+
+        for(String word : arrayOfWords) {
+            if (word.length() >= 5) {
+                listOfWords.remove(word);
+            }
+        }
+        return listOfWords.toArray(new String[0]);
+    }
+
 }
